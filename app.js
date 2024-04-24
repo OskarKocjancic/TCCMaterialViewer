f731b757f49c0dac1bb277b7fed11457c91c2bc2
OskarKocjancic <oskar.kocjancic@gmail.com>
2024-04-24 18:13:07 +0200

    fixed everything, fuck off

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-var materialLibraryURL = "https://materials.tccbuilder.org/";
-// var materialLibraryURL = "http://127.0.0.1:5555/";
+// var materialLibraryURL = "https://materials.tccbuilder.org/";
+var materialLibraryURL = "http://127.0.0.1:5555/";
 var from = document.getElementById("from");
 var to = document.getElementById("to");
 var fromSlider = document.getElementById("fromSlider");
@@ -17,108 +17,135 @@ var datasets;
 var minTemp = 0;
 var maxTemp = 2000;
 var currentShownProperty = "";
-
-fetch(urlFlags).then((response) =>
-	response.text().then((data) => {
-		var materialsList = document.querySelector(".materialsList");
-		materials = Papa.parse(data.trim(), { header: true }).data;
-		materials.forEach((m) => {
-			Object.keys(m).forEach((a) => (a !== "name" ? (m[a] = a != "" ? m[a] === "1" : m[a]) : a));
-			m.properties = ["rho"];
-			m.color = getRandomColor();
-			fetch(materialLibraryURL + m.name + "/appInfo/" + "info.json")
-				.then((response) =>
-					response.text().then((data) => {
-						Object.assign(m, JSON.parse(data));
-					})
-				)
-				.then(() => {
-					let unit = m.magnetocaloric ? "T" : m.electrocaloric ? "MVm" : "kbar";
-					if (!m.cpFields && !m.cpThysteresis) {
-						m.properties.push(`cp`);
-					} else if (!m.cpFields && m.cpThysteresis) {
-						m.properties.push(`cp_heating`);
-						m.properties.push(`cp_cooling`);
-					} else if (m.cpFields && !m.cpThysteresis)
-						m.fields.forEach((field) => {
-							if (field === "") return;
-							field = m.electrocaloric ? field : field.toFixed(2);
-							m.properties.push(`cp_${field}${unit}`);
-						});
-					else if (m.cpFields && m.cpThysteresis)
-						m.fields.forEach((field) => {
-							if (field === "") return;
-							field = m.electrocaloric ? field : field.toFixed(2);
-							m.properties.push(`cp_${field}${unit}_heating`);
-							m.properties.push(`cp_${field}${unit}_cooling`);
-						});
-					// DISABLED FOR NOW
-					// if (m.fields && m.dTThysteresis)
-					// 	m.fields.forEach((field) => {
-					// 		if (field != 0) {
-					// 			m.properties.push(`dT_${field}${unit}_apply`);
-					// 			m.properties.push(`dT_${field}${unit}_remove`);
-					// 		}
-					// 	});
-					// else if (m.fields && !m.dTThysteresis)
-					// 	m.fields.forEach((field) => {
-					// 		if (field != 0) {
-					// 			m.properties.push(`dT_${field}${unit}`);
-					// 		}
-					// 	});
-					if (m.fields) {
-						m.fields.forEach((field) => {
-							if (field === "") return;
-							field = m.electrocaloric ? field : field.toFixed(2);
-							if (field != 0) {
-								m.properties.push(`dT_${field}${unit}_apply`);
-								m.properties.push(`dT_${field}${unit}_remove`);
-							}
-							1;
-						});
-					}
-
-					if (m.kThysteresis) {
-						m.properties.push(`k_heating`);
-						m.properties.push(`k_cooling`);
-					} else m.properties.push(`k`);
-				})
-				.then(() => {
-					let materialContainer = makeMaterialContainer(m);
-					materialsList.appendChild(materialContainer);
-
-					var items = materialsList.childNodes;
-					var itemsArr = [];
-					for (var i in items) {
-						if (items[i].nodeType == 1) {
-							itemsArr.push(items[i]);
-						}
-					}
-					itemsArr.sort(function (a, b) {
-						return a.innerHTML == b.innerHTML ? 0 : a.innerHTML > b.innerHTML ? 1 : -1;
-					});
-					for (i = 0; i < itemsArr.length; ++i) {
-						materialsList.appendChild(itemsArr[i]);
-					}
-					m.properties.forEach((p) => (!selectProperties.includes(p) ? selectProperties.push(p) : p));
-				});
-		});
-
-		var select = document.querySelector(".selectProperties");
-		select.onchange = () => {
-			fetchDatasets(
-				materials.filter((a) => a.properties.includes(select.value)).map((m) => m.name + "/appInfo/" + select.value),
-				(d) => {
-					datasets = d.filter((p) => p.data.length > 1);
-					if (chart != undefined) chart.destroy();
-					chart = generateChart();
-					canvas.style.display = "block";
-					document.querySelector("#loading").style.display = "none";
-				}
-			);
-		};
+var server;
+var promisedFiles = [];
+db.open({
+	server: "database",
+	version: 1,
+	schema: {
+		files: {
+			key: { keyPath: "url" },
+			indexes: {
+				answer: { unique: true },
+			},
+		},
+	},
+})
+	.then(function (s) {
+		server = s;
 	})
-);
+	.then(
+		fetch(urlFlags).then((response) =>
+			response.text().then((data) => {
+				var materialsList = document.querySelector(".materialsList");
+				materials = Papa.parse(data.trim(), { header: true }).data;
+				let promises = [];
+				materials.forEach((m) => {
+					Object.keys(m).forEach((a) => (a !== "name" ? (m[a] = a != "" ? m[a] === "1" : m[a]) : a));
+					m.properties = ["rho"];
+					m.color = getRandomColor();
+					promises.push(
+						fetch(materialLibraryURL + m.name + "/appInfo/" + "info.json")
+							.then((response) =>
+								response.text().then((data) => {
+									Object.assign(m, JSON.parse(data));
+								})
+							)
+							.then(() => {
+								let unit = m.magnetocaloric ? "T" : m.electrocaloric ? "MVm" : "kbar";
+								if (!m.cpFields && !m.cpThysteresis) {
+									m.properties.push(`cp`);
+								} else if (!m.cpFields && m.cpThysteresis) {
+									m.properties.push(`cp_heating`);
+									m.properties.push(`cp_cooling`);
+								} else if (m.cpFields && !m.cpThysteresis)
+									m.fields.forEach((field) => {
+										if (field === "") return;
+										field = m.electrocaloric ? field : field.toFixed(2);
+										m.properties.push(`cp_${field}${unit}`);
+									});
+								else if (m.cpFields && m.cpThysteresis)
+									m.fields.forEach((field) => {
+										if (field === "") return;
+										field = m.electrocaloric ? field : field.toFixed(2);
+										m.properties.push(`cp_${field}${unit}_heating`);
+										m.properties.push(`cp_${field}${unit}_cooling`);
+									});
+								// DISABLED FOR NOW
+								// if (m.fields && m.dTThysteresis)
+								// 	m.fields.forEach((field) => {
+								// 		if (field != 0) {
+								// 			m.properties.push(`dT_${field}${unit}_apply`);
+								// 			m.properties.push(`dT_${field}${unit}_remove`);
+								// 		}
+								// 	});
+								// else if (m.fields && !m.dTThysteresis)
+								// 	m.fields.forEach((field) => {
+								// 		if (field != 0) {
+								// 			m.properties.push(`dT_${field}${unit}`);
+								// 		}
+								// 	});
+								if (m.fields) {
+									m.fields.forEach((field) => {
+										if (field === "") return;
+										field = m.electrocaloric ? field : field.toFixed(2);
+										if (field != 0) {
+											m.properties.push(`dT_${field}${unit}_apply`);
+											m.properties.push(`dT_${field}${unit}_remove`);
+										}
+										1;
+									});
+								}
+
+								if (m.kThysteresis) {
+									m.properties.push(`k_heating`);
+									m.properties.push(`k_cooling`);
+								} else m.properties.push(`k`);
+							})
+							.then(() => {
+								let materialContainer = makeMaterialContainer(m);
+								materialsList.appendChild(materialContainer);
+
+								var items = materialsList.childNodes;
+								var itemsArr = [];
+								for (var i in items) {
+									if (items[i].nodeType == 1) {
+										itemsArr.push(items[i]);
+									}
+								}
+								itemsArr.sort(function (a, b) {
+									return a.innerHTML == b.innerHTML ? 0 : a.innerHTML > b.innerHTML ? 1 : -1;
+								});
+								for (i = 0; i < itemsArr.length; ++i) {
+									materialsList.appendChild(itemsArr[i]);
+								}
+								m.properties.forEach((p) => (!selectProperties.includes(p) ? selectProperties.push(p) : p));
+							})
+					);
+				});
+				Promise.all(promises).then(() => {
+					materials.forEach((m) => fetchAllFiles(m));
+					Promise.all(promisedFiles).then((d) => {
+						document.querySelector(".loadingScreen").style.display = "none";
+					});
+				});
+
+				var select = document.querySelector(".selectProperties");
+				select.onchange = () => {
+					fetchDatasets(
+						materials.filter((a) => a.properties.includes(select.value)).map((m) => m.name + "/appInfo/" + select.value),
+						(d) => {
+							datasets = d.filter((p) => p.data.length > 1);
+							if (chart != undefined) chart.destroy();
+							chart = generateChart();
+							canvas.style.display = "block";
+							document.querySelector("#loading").style.display = "none";
+						}
+					);
+				};
+			})
+		)
+	);
 
 function makeMaterialContainer(m) {
 	var materialContainer = document.createElement("div");
@@ -326,81 +353,212 @@ function reset() {
 	document.querySelectorAll("button").forEach((button) => button.classList.remove("activeMaterialProperty"));
 }
 
-function fetchDatasets(names, callback) {
+// async function getFile(name, url) {
+// 	// check if file is already in database and return it
+// 	// else fetch it and add it to the database
+// 	return server.files.get(name).then(function (results) {
+// 		if (results !== undefined) {
+// 			console.log(`${name} already in database`);
+// 			return results.data;
+// 		} else {
+// 			return fetch(url)
+// 				.then((response) => {
+// 					if (!response.ok) {
+// 						console.log(name + " not found", response);
+// 						return "0.0\n".repeat(20000);
+// 					} else return response.text();
+// 				})
+// 				.then((data) => {
+// 					server.files.add({
+// 						url: name,
+// 						data: data,
+// 					});
+// 					console.log(`added ${name} database`);
+// 					return data;
+// 				});
+// 		}
+// 	});
+// }
+function getFile(name, url) {
+	return new Promise((resolve, reject) => {
+		server.files.get(name).then(function (results) {
+			if (results !== undefined) {
+				// console.log(`${name} already in database`);
+				resolve(results.data);
+			} else {
+				fetch(url)
+					.then((response) => {
+						if (!response.ok) {
+							console.log(name + " not found");
+							return "0.0\n".repeat(20000);
+						} else return response.text();
+					})
+					.then((data) => {
+						server.files.add({
+							url: name,
+							data: data,
+						});
+						console.log(`added ${name} database`);
+						resolve(data);
+					})
+					.catch((error) => reject(error));
+			}
+		});
+	});
+}
+
+async function fetchAllFiles(m) {
+	m.properties.forEach((p) => {
+		promisedFiles.push(getFile(m.name + "/appInfo/" + p + ".txt", materialLibraryURL + m.name + "/appInfo/" + p + ".txt"));
+		promisedFiles[promisedFiles.length - 1].then((data) => {});
+	});
+}
+
+async function fetchDatasets(names, callback) {
 	document.querySelector("#loading").style.display = "block";
 	canvas.style.display = "none";
 	datasets = names.map(async (name) => {
 		const url = materialLibraryURL + name + ".txt";
-		return fetch(url)
-			.then((response) => {
-				if (!response.ok) {
-					console.log(name + " not found");
-					return "0.0\n".repeat(20000);
+		// server.files.get(name + ".txt").then(function (results) {
+		// 	if (results !== undefined) {
+		// 		return fetch(url)
+		// 			.then((response) => {
+		// 				if (!response.ok) {
+		// 					console.log(name + " not found");
+		// 					return "0.0\n".repeat(20000);
+		// 				}
+
+		// 				return response.text();
+		// 			})
+		// 			.then((data) => {
+		// 				server.files.add({
+		// 					url: name + ".txt",
+		// 					data: data,
+		// 				});
+		// 				return data;
+		// 			});
+		// 	} else {
+		// 		var dataPoints = data.trim().split("\n");
+		// 		dataPoints = dataPoints.map((a) => parseFloat(a));
+
+		// 		var material = materials.find((m) => m.name === name.split("/")[0]);
+		// 		var value = name.split("/")[2].split("_")[0];
+		// 		var map = {
+		// 			cp: "specificHeatCapacity",
+		// 			rho: "density",
+		// 			k: "thermalConductivity",
+		// 			dT: "adiabaticTemperatureChange",
+		// 		};
+		// 		let rangeString = material.ranges[map[value]];
+		// 		var min = rangeString !== "" && rangeString != undefined ? parseFloat(rangeString.split("-")[0]) : 0;
+		// 		var max = rangeString !== "" && rangeString != undefined ? parseFloat(rangeString.split("-")[1]) : 2000;
+		// 		var newDataPoints = [];
+
+		// 		if (dataPoints.length == 1) {
+		// 			for (let i = 0; i < maxTemp; i++) {
+		// 				if (i > 285 && i < 302) newDataPoints.push(dataPoints[0]);
+		// 				else newDataPoints.push(null);
+		// 				labels.push(i);
+		// 			}
+		// 		} else {
+		// 			let range = maxTemp - minTemp;
+		// 			let modulo = (range * 10) / 25;
+		// 			modulo = 10;
+		// 			for (let i = 0; i < dataPoints.length; i++) {
+		// 				if (i % modulo == 0) {
+		// 					newDataPoints.push(dataPoints[i]);
+		// 					labels.push(i * 0.1);
+		// 				}
+		// 			}
+		// 		}
+		// 		for (let i = 0; i < dataPoints.length; i++) {
+		// 			if (newDataPoints[i] > 15000) newDataPoints[i] = 16000;
+		// 			if (newDataPoints[i] < -15000) newDataPoints[i] = -16000;
+		// 			if (!(i > min && i < max) || newDataPoints[i] == 0) newDataPoints[i] = null;
+		// 		}
+		// 		material.shadeCounter++;
+
+		// 		//get number of properties that start with value
+		// 		properties = material.properties.filter((p) => p.split("_")[0] === value);
+
+		// 		return {
+		// 			type: "line",
+		// 			indexAxis: "x",
+		// 			label: names != shownFiles ? name.split("/")[0] : name.replace("/appInfo/", " - "),
+		// 			data: newDataPoints.map((a, i) => {
+		// 				return {
+		// 					x: i,
+		// 					y: a,
+		// 				};
+		// 			}),
+		// 			fill: false,
+		// 			borderColor: getShadeOfColor(material.color, 1 + (material.shadeCounter / properties.length + 1)),
+		// 			backgroundColor: getShadeOfColor(material.color, 1 + (material.shadeCounter / properties.length + 1)),
+		// 			tension: 0.1,
+		// 			value: value,
+		// 		};
+		// 	}
+		// });
+		var data = await getFile(name + ".txt", url);
+		var dataPoints = data.trim().split("\n");
+		dataPoints = dataPoints.map((a) => parseFloat(a));
+
+		var material = materials.find((m) => m.name === name.split("/")[0]);
+		var value = name.split("/")[2].split("_")[0];
+		var map = {
+			cp: "specificHeatCapacity",
+			rho: "density",
+			k: "thermalConductivity",
+			dT: "adiabaticTemperatureChange",
+		};
+		let rangeString = material.ranges[map[value]];
+		var min = rangeString !== "" && rangeString != undefined ? parseFloat(rangeString.split("-")[0]) : 0;
+		var max = rangeString !== "" && rangeString != undefined ? parseFloat(rangeString.split("-")[1]) : 2000;
+		var newDataPoints = [];
+
+		if (dataPoints.length == 1) {
+			for (let i = 0; i < maxTemp; i++) {
+				if (i > 285 && i < 302) newDataPoints.push(dataPoints[0]);
+				else newDataPoints.push(null);
+				labels.push(i);
+			}
+		} else {
+			let range = maxTemp - minTemp;
+			let modulo = (range * 10) / 25;
+			modulo = 10;
+			for (let i = 0; i < dataPoints.length; i++) {
+				if (i % modulo == 0) {
+					newDataPoints.push(dataPoints[i]);
+					labels.push(i * 0.1);
 				}
+			}
+		}
+		for (let i = 0; i < dataPoints.length; i++) {
+			if (newDataPoints[i] > 15000) newDataPoints[i] = 16000;
+			if (newDataPoints[i] < -15000) newDataPoints[i] = -16000;
+			if (!(i > min && i < max) || newDataPoints[i] == 0) newDataPoints[i] = null;
+		}
+		material.shadeCounter++;
 
-				return response.text();
-			})
-			.then((data) => {
-				var dataPoints = data.trim().split("\n");
-				dataPoints = dataPoints.map((a) => parseFloat(a));
+		//get number of properties that start with value
+		properties = material.properties.filter((p) => p.split("_")[0] === value);
 
-				var material = materials.find((m) => m.name === name.split("/")[0]);
-				var value = name.split("/")[2].split("_")[0];
-				var map = {
-					cp: "specificHeatCapacity",
-					rho: "density",
-					k: "thermalConductivity",
-					dT: "adiabaticTemperatureChange",
-				};
-				let rangeString = material.ranges[map[value]];
-				var min = rangeString !== "" && rangeString != undefined ? parseFloat(rangeString.split("-")[0]) : 0;
-				var max = rangeString !== "" && rangeString != undefined ? parseFloat(rangeString.split("-")[1]) : 2000;
-				var newDataPoints = [];
-
-				if (dataPoints.length == 1) {
-					for (let i = 0; i < maxTemp; i++) {
-						if (i > 285 && i < 302) newDataPoints.push(dataPoints[0]);
-						else newDataPoints.push(null);
-						labels.push(i);
-					}
-				} else {
-					let range = maxTemp - minTemp;
-					let modulo = (range * 10) / 25;
-					modulo = 10;
-					for (let i = 0; i < dataPoints.length; i++) {
-						if (i % modulo == 0) {
-							newDataPoints.push(dataPoints[i]);
-							labels.push(i * 0.1);
-						}
-					}
-				}
-				for (let i = 0; i < dataPoints.length; i++) {
-					if (newDataPoints[i] > 15000) newDataPoints[i] = 16000;
-					if (newDataPoints[i] < -15000) newDataPoints[i] = -16000;
-					if (!(i > min && i < max) || newDataPoints[i] == 0) newDataPoints[i] = null;
-				}
-				material.shadeCounter++;
-
-				//get number of properties that start with value
-				properties = material.properties.filter((p) => p.split("_")[0] === value);
-
+		return {
+			type: "line",
+			indexAxis: "x",
+			label: names != shownFiles ? name.split("/")[0] : name.replace("/appInfo/", " - "),
+			data: newDataPoints.map((a, i) => {
 				return {
-					type: "line",
-					indexAxis: "x",
-					label: names != shownFiles ? name.split("/")[0] : name.replace("/appInfo/", " - "),
-					data: newDataPoints.map((a, i) => {
-						return {
-							x: i,
-							y: a,
-						};
-					}),
-					fill: false,
-					borderColor: getShadeOfColor(material.color, 1 + (material.shadeCounter / properties.length + 1)),
-					backgroundColor: getShadeOfColor(material.color, 1 + (material.shadeCounter / properties.length + 1)),
-					tension: 0.1,
-					value: value,
+					x: i,
+					y: a,
 				};
-			});
+			}),
+			fill: false,
+			borderColor: getShadeOfColor(material.color, 1 + (material.shadeCounter / properties.length + 1)),
+			backgroundColor: getShadeOfColor(material.color, 1 + (material.shadeCounter / properties.length + 1)),
+			tension: 0.1,
+			value: value,
+		};
 	});
 
 	Promise.all(datasets).then(callback);
@@ -485,7 +643,8 @@ function syncSlidersWithInputs() {
 	if (to.value > 2000) to.value = 2000;
 	if (to.value < 0) to.value = 0;
 	if (from.value > 2000) from.value = 2000;
-	if (from.value < 0) from.value = 0;1
+	if (from.value < 0) from.value = 0;
+	1;
 	fromSlider.value = from.value;
 	toSlider.value = to.value;
 	// if (from.value > to.value) {
